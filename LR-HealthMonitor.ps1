560b8104deb2c2cb86391178c3572a9f88d02dd1
MikeC-I <mike.ci@gmail.com>
2019-03-26 12:18:45 -0400

    Added cluster monitoring and optimizations
    2 large changes:
    1. Added indexer cluster monitoring which will check for status (green/yellow/red), number of nodes in the cluster, and percentage of active shards.
    2. Optimized service WMI requests to grab statuses for all services in one request, significantly reducing the number of requests and thus time spent reading service status

--- LR-HealthMonitor.ps1
+++ LR-HealthMonitor.ps1
@@ -31,7 +31,7 @@ $smtpserver = $hosts.config.smtpserver
 $mailfrom = $hosts.config.mailfrom
 $mailrecipients = @($hosts.config.recipients)
 $reportsubject = $hosts.config.reportsubject
-$warninsubject = $hosts.config.warningsubject
+$warningsubject = $hosts.config.warningsubject
 
 Function Get-DriveStatus([String]$hostname, [String]$driveletter) {
     Try {
@@ -46,13 +46,13 @@ Function Get-DriveStatus([String]$hostname, [String]$driveletter) {
     }
 }
 
-Function Get-ServiceStatus([String]$hostname, [String]$servicename) {
+Function Get-ServerServices([String]$hostname) {
     Try {
-        $thisservice = Get-WmiObject Win32_service -Computer $hostname -Filter "DisplayName='$servicename'"
-        $thisservice.State
+        $serverservices = Get-WmiObject Win32_service -Computer $hostname
+        $serverservices
     }
     Catch {
-        "Error retrieving service status"
+        "Error retrieving server services"
     }
 }
 
@@ -66,6 +66,17 @@ Function Get-DirFileCount($hostname, $directory) {
     "Error Reading Remote Directory"
     }
 }
+
+Function Get-ClusterInfo($hostname) {
+    Try {
+        $dxstatus = Invoke-RestMethod "http://$($hostname):9200/_cluster/health?level=indices&pretty"
+        $dxstatus
+    }
+    Catch {
+        "Error retreiving cluster statistics"
+    }
+}
+    
 
 Function Get-HostsData($hostjson) {
     $hoststatus = @()
@@ -83,9 +94,10 @@ Function Get-HostsData($hostjson) {
             $drives += $thisdrive
         }
         $services = @()
+        $serverservices = Get-ServerServices($Hostname)
         ForEach ($service in $_.services) {
             $thisservice = New-Object -TypeName psobject
-            $thisservicestatus = Get-ServiceStatus $Hostname $service
+            $thisservicestatus = $($serverservices | where DisplayName -eq $service).State
             $thisservice | Add-Member -MemberType NoteProperty -Name ServiceName -Value $service
             $thisservice | Add-Member -MemberType NoteProperty -Name ServiceStatus -Value $thisservicestatus
             $services += $thisservice
@@ -107,10 +119,23 @@ Function Get-HostsData($hostjson) {
     $hoststatus
 }
 
-Function Write-Hosts($hoststatus) {
+Function Get-ClustersStatus($hostjson) {
+    $clusterstatus = @()
+    $hostjson.clusters | ForEach-Object {
+        $thisCluster = New-Object -TypeName psobject
+        $cluster = Get-ClusterInfo $_.clusteraddress
+        $thisCluster | Add-Member -MemberType NoteProperty -Name Cluster -Value $_.clustername
+        $thisCluster | Add-Member -MemberType NoteProperty -Name Nodes -Value $cluster.number_of_nodes 
+        $thisCluster | Add-Member -MemberType NoteProperty -Name Status -Value $cluster.status
+        $thisCluster | Add-Member -MemberType NoteProperty -Name ActivePercent -Value $cluster.active_shards_percent_as_number
+        $clusterstatus += $thisCluster
+    }
+    $clusterstatus
+}
+
+Function Write-Hosts($hoststatus,$clusterstatus) {
     $hoststatus | ForEach-Object {
-        Write-Output "Hostname:"
-        Write-Output $_.Hostname
+        Write-Output "Hostname:",$_.Hostname
         Write-Output $_.Drives | Format-Table -AutoSize
         Write-Output $_.Services | Format-Table -AutoSize
         if ($_.PSobject.Properties.Name -contains "DXRPCount") {
@@ -122,6 +147,8 @@ Function Write-Hosts($hoststatus) {
             Write-Output " "
         }
     }
+    Write-Output "Indexer Clusters:"
+    Write-Output $clusterstatus | Format-Table -AutoSize
 }
 
 Function Get-Warnings($hoststatus) {
@@ -160,51 +187,101 @@ Function Get-Warnings($hoststatus) {
     $hostwarnings        
 }
 
-Function Write-Warnings($hostwarnings) {
-    if ($hostwarnings.Count -eq 0) { break }
-    Write-Output "Warnings are present for the following hosts:"
-    Write-Output " "
-    $hostwarnings | ForEach-Object {
-        Write-Output $_.Hostname
-        Write-Output ("-" * $_.Hostname.length)
+Function Get-ClusterWarnings($clusterstatus) {
+    $clusterwarns = @()
+    $clusterstatus | ForEach-Object {
+        $statuswarnings = @()
+        $nodeswarnings = @()
+        $shardswarnings = @()
+        if ($_.Nodes -lt ($hosts.clusters | where clustername -eq $_.Cluster).numberofnodes) { $nodeswarnings += $_.Nodes }
+        if ($_.Status -ne "green") { $statuswarnings += $_.Status }
+        if ($_.ActivePercent -lt 100) { $shardswarnings += $_.ActivePercent }
+        if (($statuswarnings.Count -gt 0) -or ($nodeswarnings.Count -gt 0) -or ($shardswarnings.Count -gt 0)) {
+            $warncluster = New-Object -TypeName psobject
+            $warncluster | Add-Member -MemberType NoteProperty -Name Cluster -Value $_.Cluster
+            if ($statuswarnings.Count -gt 0) { $warncluster | Add-Member -MemberType NoteProperty -Name Status -Value $statuswarnings }
+            if ($nodeswarnings.Count -gt 0) { $warncluster | Add-Member -MemberType NoteProperty -Name Nodes -Value $nodeswarnings }
+            if ($shardswarnings.Count -gt 0) { $warncluster | Add-Member -MemberType NoteProperty -Name ActiveShards -Value $shardsswarnings }
+        }
+        $clusterwarns += $warncluster
+    }
+    $clusterwarns
+}
+
+Function Write-Warnings($hostwarnings, $clusterwarnings) {
+    if (($hostwarnings.Count -eq 0) -and ($clusterwarnings.Count -eq 0)) { break }
+    if ($hostwarnings.Count -ne 0) {
+        Write-Output "Warnings are present for the following hosts:"
         Write-Output " "
-        if ($_.Drives.Count -gt 0) {
-            Write-Output "Free space on one or more drives is below the threshold:"
-            Write-Output $_.Drives | Format-Table -AutoSize
-         Write-Output " "
-        }
-        if ($_.Services.Count -gt 0) {
-            Write-Output "One or more monitored services is not in a running state:"
-            Write-Output $_.Services | Format-Table -AutoSize
+        $hostwarnings | ForEach-Object {
+            Write-Output $_.Hostname
+            Write-Output ("-" * $_.Hostname.length)
             Write-Output " "
+            if ($_.Drives.Count -gt 0) {
+                Write-Output "Free space on one or more drives is below the threshold:"
+                Write-Output $_.Drives | Format-Table -AutoSize
+             Write-Output " "
+            }
+            if ($_.Services.Count -gt 0) {
+                Write-Output "One or more monitored services is not in a running state:"
+                Write-Output $_.Services | Format-Table -AutoSize
+                Write-Output " "
+            }
+            if ($_.DXReliablePersist.Count -gt 0) {
+                Write-Output "File count in the DXReliablePersist folder is above the threshold:"
+                Write-Output $_.DXReliablePersist
+                Write-Output " "
+            }
+            if ($_.AIEData.Count -gt 0) { 
+                Write-Output "File count in the AIE data folder is above the threshold:"
+                Write-Output $_.AIEData
+                Write-Output " "
+            }
         }
-        if ($_.DXReliablePersist.Count -gt 0) {
-            Write-Output "File count in the DXReliablePersist folder is above the threshold:"
-            Write-Output $_.DXReliablePersist
+    }
+    if ($clusterwarnings.Count -ne 0) {
+        Write-Output "Warnings are present for the following clusters:"
+        Write-Output " "
+        $clusterwarnings | ForEach-Object {
+            Write-Output $_.Cluster
+            Write-Output ("-" * $_.Cluster.length)
             Write-Output " "
-        }
-        if ($_.AIEData.Count -gt 0) {
-            Write-Output "File count in the AIE data folder is above the threshold:"
-            Write-Output $_.AIEData
-            Write-Output " "
+            if ($_.Status.Count -gt 0) {
+                Write-Output  "The status of the cluster is not green.  Current status: $($_.Status)"
+                Write-Output " "
+            }
+            if ($_.Nodes.Count -gt 0) {
+                Write-Output "The number of active nodes is less than total nodes for this cluster"
+                Write-Output "Active Nodes: $($_.Nodes)"
+                Write-Output "Total Nodes: $( ($hosts.clusters | where clustername -eq $_.Cluster).numberofnodes )"
+                Write-Output " "
+            }
+            if ($_.Shards.Count -gt 0) {
+                Write-Output "The percent of active shards is below 100%"
+                Write-Output "Current Percent: $($_.Shards)"
+                Write-Output " "
+            }
         }
     }
 }
     
 
-Function Email-Warnings($hostwarnings) {
-    $msgbody = Write-Warnings $hostwarnings | Out-String
-    Send-MailMessage -SmtpServer $smtpserver -From $mailfrom -To $mailrecipients -Subject $warninsubject -Body $msgbody
+Function Email-Warnings($hostwarnings,$clusterwarnings) {
+    $msgbody = Write-Warnings $hostwarnings $clusterwarnings | Out-String
+    Send-MailMessage -SmtpServer $smtpserver -From $mailfrom -To $mailrecipients -Subject $warningsubject -Body $msgbody
 }
 
-Function Email-Report($hoststatus) {
-    $msgbody = Write-Hosts $hoststatus | Out-String
+Function Email-Report($hoststatus,$clusterstatus) {
+    $msgbody = Write-Hosts $hoststatus $clusterstatus | Out-String
     Send-MailMessage -SmtpServer $smtpserver -From $mailfrom -To $mailrecipients -Subject $reportsubject -Body $msgbody
 }
 
 $allhoststatus = Get-HostsData $hosts
+$allclusterstatus = Get-ClustersStatus $hosts
 $warns = Get-Warnings $allhoststatus
-if ($EmailWarnings.IsPresent) { Email-Warnings $warns }
-if ($EmailReport.IsPresent) { Email-Report $allhoststatus }
-if ($OutputReport.IsPresent) { Write-Hosts $allhoststatus }
-if ($OutputWarnings.IsPresent) { Write-Warnings $warns }
+$clusterwarnings = Get-ClusterWarnings $allclusterstatus
+
+if ($EmailWarnings.IsPresent) { Email-Warnings $warns $clusterwarnings }
+if ($EmailReport.IsPresent) { Email-Report $allhoststatus $allclusterstatus }
+if ($OutputReport.IsPresent) { Write-Hosts $allhoststatus $allclusterstatus }
+if ($OutputWarnings.IsPresent) { Write-Warnings $warns $clusterwarnings }
